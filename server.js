d272d8ff03ec95994ecc9562fa3957f9e826358d
TJ <tj@ojingolabs.com>
2013-08-14 18:52:28 +0400

    with size displayed of file being pushed and a sample image

--- server.js
+++ server.js
@@ -14,8 +14,29 @@ var root = __dirname;
 var server = http.createServer(function(req, res) {
     var url = parse(req.url);
     var path = join(root, url.pathname);
-    var stream = fs.createReadStream(path);
-    stream.pipe(res);
+    fs.stat(path, function(err, stat) {
+        if(err) {
+            if('ENOENT' == err.code ) {
+                res.statusCode = 404;
+                res.end('Not found!\n');
+                console.log('Not found 404!');
+            } else {
+                res.statusCode = 500;
+                res.end('Internal Servers Errors!');
+            }
+        } else {
+            res.setHeader('Content-Length', stat.size);
+            var stream = fs.createReadStream(path);
+            stream.pipe(res);
+            console.log("File is being served? stat.size = " + stat.size);
+            stream.on('error', function(err) {
+                res.statusCode = 500;
+                res.end('Internal Server Errors YO!\n');
+                console.log("Internal Server Error Triggered YO! stat.size = " + stat.size + "\n");
+                console.log(err);
+            });
+        }
     });
+});
 server.listen(3000);
 
